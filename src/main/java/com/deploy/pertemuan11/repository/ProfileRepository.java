b677306991b0f125c026d7860826363bc555627c
mufid1012 <mufidghibran1012@gmail.com>
2026-05-20 10:00:51 +0700

    Add profile repository

--- src/main/java/com/deploy/pertemuan11/repository/ProfileRepository.java
+++ src/main/java/com/deploy/pertemuan11/repository/ProfileRepository.java
@@ -1,4 +1,7 @@
 package com.deploy.pertemuan11.repository;
 
-public interface ProfileRepository {
+import com.deploy.pertemuan11.model.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileRepository extends JpaRepository<Profile, String> {
 }
